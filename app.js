85221588151e13fc361ce0e923206fe6f2521fef
Vishnu <23cs066@nandhaengg.org>
2026-04-25 22:05:02 +0530

    testing commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello from DevOps CI/CD 🚀");
+    res.send("just for testying");
 });
 
 app.listen(3000, () => {
